5ca74c902008e2379edb5ab979c12536fc86740c
Upasana Sharma <upasanasharma@192.168.1.2>
2021-12-17 13:51:50 +0530

    edit main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "google_compute_instance" "cb_test_vm" {
   name         = "cb_test_vm"
   machine_type = "e2-medium"
   zone         = "asia-south1-b"
+  project = "searce-playground"
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
